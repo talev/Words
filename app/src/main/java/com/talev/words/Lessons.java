b67d2a6d0257f0605217c71f799c6e370062c172
talev@ifao.net <talev@ifao.net>
2016-07-07 09:17:48 +0300

    Add kvtml elements

---
+++ app/src/main/java/com/talev/words/Lessons.java
@@ -0,0 +1,19 @@
+package com.talev.words;
+
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
+
+import java.util.List;
+
+/**
+ * Created by Dimko Talev on 07.07.2016 г.
+ */
+@Element(name = "lessons")
+public class Lessons {
+
+/*
+    @ElementList
+    public List<Container> containers;
+*/
+
+}
