27e122d040e33c0827ba1600d76d94ff53a6a001
talev@ifao.net <talev@ifao.net>
2016-07-18 13:51:59 +0300

    Code reformat

--- app/src/main/java/com/talev/words/MainActivity.java
+++ app/src/main/java/com/talev/words/MainActivity.java
@@ -196,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v.getId() == R.id.btn_check_it) {
             if (itTranslated) {
                 showFirstWord();
-            } else  {
+            } else {
                 showSecondWord();
             }
         }
